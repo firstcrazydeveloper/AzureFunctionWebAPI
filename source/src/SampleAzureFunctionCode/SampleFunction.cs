99ff00a980549c0873c1edfb2556dc5c4c0b6e1c
Abhishek Kumar <abhishekar@HCL.COM>
2020-02-12 18:43:48 +0530

    Add new Message Pipeline

--- source/src/SampleAzureFunctionCode/SampleFunction.cs
+++ source/src/SampleAzureFunctionCode/SampleFunction.cs
@@ -68,5 +68,24 @@ namespace SampleAzureFunctionCode
                 ? (ActionResult)new OkObjectResult($"Hello, {name}")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
+
+        [HttpGet]
+        [FunctionName(nameof(GetMessagePipelineAsync))]
+        public async Task<IActionResult> GetMessagePipelineAsync(
+            [HttpTrigger(AuthorizationLevel.Function, "GET", Route = "agents")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            return name != null
+                ? (ActionResult)new OkObjectResult($"Hello, {name}")
+                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        }
     }
 }
