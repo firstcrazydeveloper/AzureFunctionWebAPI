0f9e149992b52b00f6bcfcb934dad25450074207
Abhishek Kumar <abhishekar@HCL.COM>
2020-02-12 19:13:09 +0530

    Add Test files

---
+++ source/tests/SampleAzureFunctionCode.Tests/SampleFunctionSpecs.cs
@@ -0,0 +1,18 @@
+namespace SampleAzureFunctionCode.Tests
+{
+    using NUnit.Framework;
+
+    public class SampleFunctionSpecs
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestSampleFunction()
+        {
+            Assert.Pass();
+        }
+    }
+}
