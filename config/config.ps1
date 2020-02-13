e1815c32a1727878e81c24a218c0ccc0c00a9176
Abhishek Kumar <abhishekar@HCL.COM>
2020-02-13 13:06:46 +0530

    Update login concepts

--- config/config.ps1
+++ config/config.ps1
@@ -1,14 +1,12 @@
 # Register Resource Providers if they're not already registered
-$userId = "sahil.abhishek777@hotmail.com"
-$passwd = ConvertTo-SecureString Abhinutan2018 -AsPlainText -Force
-$cred = New-Object -TypeName System.Management.Automation.PSCredential($userId ,$passwd)
+
 $resourceGroupName = "fcd-dev-webapp-abc"
 $functionAppName = "fcd-dev-web-app-abc"
 
 # Create the parameters for the file, which for this template is the function app name.
 $TemplateParams = @{"appName" = $functionAppName}
 
-Login-AzAccount -Credential $cred -TenantId "09abbc86-84ed-4481-83a3-4dc0f095d0b9"
+
 Register-AzResourceProvider -ProviderNamespace "microsoft.web"
 Register-AzResourceProvider -ProviderNamespace "microsoft.storage"
 
